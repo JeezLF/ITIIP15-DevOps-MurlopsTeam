5adbdf8721d9662f37eb8f860aed1fe22593b142
GRANDIN Leo <Leo.GRANDIN@akka.eu>
2019-11-29 11:40:47 +0100

    Mise en place de la classe rangeTest

---
+++ retwisj/src/test/java/retwisj/RangeTest.java
@@ -0,0 +1,22 @@
+package retwisj;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.data.redis.samples.retwisj.Range;
+
+public class RangeTest {
+	
+	@Test
+	public void rangePageNumber() {
+		Range range = new Range(10);
+		assertEquals(10, range.getPages());
+	}
+	
+	@Test
+	public void rangePageBeginEnd() {
+		Range range = new Range(100,30);
+		assertEquals(3, range.getPages());
+	}
+	
+}
