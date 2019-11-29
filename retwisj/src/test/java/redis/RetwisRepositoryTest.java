a80fcdeb0af6e2e58aa0c3477d15dd1d5820e6f2
zoouum <zoouum@LAPTOP-GVF64A3G.localdomain>
2019-11-29 10:46:31 +0100

    JUnit

--- retwisj/src/test/java/redis/RetwisRepositoryTest.java
+++ retwisj/src/test/java/redis/RetwisRepositoryTest.java
@@ -1,16 +1,25 @@
 package redis;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.data.redis.samples.retwisj.redis.RetwisRepository;
 
 public class RetwisRepositoryTest {
 
+	@Before
+	public void init() {
+		//RetwisRepository retwisRepository = new RetwisRepository("");
+	}
+	
 	@Test
 	public void test() {
 		assert true;
 	}
 	
 	@Test
-	public void addUser() {
-		String name = "Clement";
+	public void addUserTest() {
+		String name = "admin";
+		String password = "password";
+		System.out.println(name + " " + password);
 		
 		
 	}
