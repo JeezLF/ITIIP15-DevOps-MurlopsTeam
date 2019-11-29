e4d58fd31474a3654f4effaa0579eb3c863b950a
GRANDIN Leo <Leo.GRANDIN@akka.eu>
2019-11-29 11:22:36 +0100

    classe de test pour Retwis security

--- retwisj/src/test/java/retwisj/RetwisSecurityTest.java
+++ retwisj/src/test/java/retwisj/RetwisSecurityTest.java
@@ -1,13 +1,37 @@
 package retwisj;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.data.redis.samples.retwisj.RetwisSecurity;
 
 public class RetwisSecurityTest {
 
-	@Test
-	public void test() {
-		assert true;
+	@Before
+	public void initConnexion() {
+		RetwisSecurity.setUser("John", "Do");
 	}
 	
+	@Test
+	public void testConnexion() {
+		assertEquals(RetwisSecurity.getName(), "John");
+		assertEquals(RetwisSecurity.getUid(), "Do");  
+	}
 	
+	@Test
+	public void testUserSignedIn() {
+		assert RetwisSecurity.isUserSignedIn(RetwisSecurity.getName());
+	}
+	
+	@Test
+	public void testUserIsSigned() {
+		assert RetwisSecurity.isSignedIn();
+	}
+	
+	@Test
+	public void testUseClean() {
+		RetwisSecurity.clean();
+		assertEquals(null,RetwisSecurity.getName());
+	}
 
 }
